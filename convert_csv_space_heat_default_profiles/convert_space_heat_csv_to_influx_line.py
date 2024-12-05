23bc81df2d9bb84a437bb79464b9c4bc8338c023
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-12-05 17:08:58 +0100

    1: Add space heat profiles.

---
+++ convert_csv_space_heat_default_profiles/convert_space_heat_csv_to_influx_line.py
@@ -0,0 +1,41 @@
+from collections import OrderedDict
+import csv
+import datetime
+
+influx_measurement_name = 'Space\\ Heat\\ default\\ profiles'
+time_column_name = 'time'
+files_with_measurement_name = {'SpaceHeat_and_HotWater_NormalizedPowerProfile_1700_1950.csv': 'SpaceHeat_and_HotWater',
+         'SpaceHeat_and_HotWater_NormalizedPowerProfile_1900_2000.csv': 'SpaceHeat_and_HotWater',
+         'SpaceHeat_and_HotWater_NormalizedPowerProfile_2000_2010.csv': 'SpaceHeat_and_HotWater',
+         'SpaceHeat_NormalizedPowerProfile_1700_1950.csv': 'SpaceHeat',
+         'SpaceHeat_NormalizedPowerProfile_1900_2000.csv': 'SpaceHeat',
+         'SpaceHeat_NormalizedPowerProfile_2000_2010.csv': 'SpaceHeat'}
+
+
+dataframe = OrderedDict()
+
+for file_name, measurement_name in files_with_measurement_name.items():
+    field_key = file_name.rstrip('.csv')
+
+    with open(file_name) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        for row in csv_reader:
+            time = datetime.datetime.fromisoformat(row[time_column_name])
+            dataframe.setdefault(time, OrderedDict())[field_key] = row[measurement_name]
+
+
+with open('../influxdb/energy_profiles.space_heat_default_profiles.influx_line', 'w+') as influx_line_file:
+    influx_line_file.write('''# DDL
+CREATE DATABASE energy_profiles
+
+# DML
+# CONTEXT-DATABASE: energy_profiles
+''')
+
+    for time, fields_with_values in dataframe.items():
+        fields = []
+        for field_name, measurement in fields_with_values.items():
+            fields.append(f'{field_name}={measurement}')
+
+        influx_line_file.write(f'{influx_measurement_name} {",".join(fields)} {int(time.timestamp()) * 1_000_000_000}\n')
